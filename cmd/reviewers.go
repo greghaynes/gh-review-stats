6b5a0056888f56b0af963000fc9707dac40a0719
Doug Hellmann <doug@doughellmann.com>
2021-04-25 07:33:53 -0400

    reviewers: update help text
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- cmd/reviewers.go
+++ cmd/reviewers.go
@@ -24,15 +24,9 @@ import (
 // reviewersCmd represents the reviewers command
 var reviewersCmd = &cobra.Command{
 	Use:   "reviewers",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("reviewers called")
+	Short: "List reviewers of PRs in a repo",
 	},
 }
 
