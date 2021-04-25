020362ae7407e1aac53538c11177cb1c08b34196
Doug Hellmann <doug@doughellmann.com>
2021-04-25 07:03:29 -0400

    make default config file name match config type
    The generated help text for the --config flag doesn't match the type.
    
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- cmd/root.go
+++ cmd/root.go
@@ -55,7 +55,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gh-review-stats.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gh-review-stats.yml)")
 
 }
 
